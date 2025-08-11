95dc508a9af84480d2345bd15eb79928244d2c60
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-08-11 15:01:16 -0400

    Finalize NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect, useContext, } from 'react'
+import { UserContext } from '../App';
+import { useNavigate, Outlet } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+
+function NavBar({token, handleLogOut}) {
+    const navigate = useNavigate();
+
+
+
+
+    return(
+        <nav className='navbar navbar-dark px-4 bg-dark'>
+            <div className='navbar-brand fs-2'>Blog Manager</div>
+            {!token && (
+                <button className="btn btn-primary text fs-5"
+                onClick={() => navigate("/login")}>Log In</button>
+            )}
+            {token && (
+                <button className="btn btn-danger text fs-5"
+                onClick={handleLogOut}>Log Out</button>
+            )}
+        </nav>
+    );
+}
+
+
+
+export default NavBar
