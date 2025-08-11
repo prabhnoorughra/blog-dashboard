6a6b3f9eebe6b4c5cd8d2ebb9eea12fc2391e00d
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-08-11 15:00:56 -0400

    Create HomePage Paths before implementing the various pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,11 @@ import ErrorPage from "./components/ErrorPage"
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import HomePage from "./components/HomePage";
+import PostListDisplay from "./components/PostListDisplay";
+import NewPost from "./components/NewPost";
+import EditPost from "./components/EditPost";
+import CommentList from "./components/CommentList";
+import RepliesList from "./components/RepliesList";
 
 
 
@@ -12,7 +17,16 @@ const routes = [
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
-            {index: true, element: <HomePage />},
+            {
+                element: <HomePage />,
+                children: [
+                    { index: true,                                      element: <PostListDisplay /> },
+                    { path: 'new',                                      element: <NewPost /> },
+                    { path: 'edit/:postId',                             element: <EditPost /> },  
+                    { path: 'comments/:postId',                         element: <CommentList /> },    
+                    { path: 'comment/:postId/:commentId',               element: <RepliesList /> },
+                ]
+            },
             {path: "login", element: <LoginPage />},
             {path: "sign-up", element: <SignupPage />},
         ],
