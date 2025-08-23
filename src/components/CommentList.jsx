aa659820d1c4ac078cdac4d5de0f08c05a0b1853
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-08-23 16:35:25 -0400

    Utilize new components in main pages and remove the need for a separate RepliesList page

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -4,14 +4,9 @@ import { useNavigate, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import LoadPage from './LoadPage';
 import ServerErrorPage from './ServerErrorPage';
+import Comment from './Comment';
+import Pagination from './Pagination';
 
-const dateFmt = new Intl.DateTimeFormat(undefined, {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-});
-
-const formatDate = (d) => d ? dateFmt.format(new Date(d)) : '';
 
 
 function CommentList() {
@@ -19,15 +14,19 @@ function CommentList() {
     const {token, changeToken, user} = useContext(UserContext); 
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [comments, setComments] = useState([]);
-    const { postId } = useParams();
+    const [data, setData] = useState();
+    const { postId, commentId } = useParams();
 
+    const [page, setPage] = useState(1);
     const apiURL = import.meta.env.VITE_API_URL;
+    const take = 10;
+
+    const arr = data ? (data.comments || data.comment.replies) : [];
 
     const fetchComments = useCallback(async () => {
         setLoading(true);
         try {
-            const response = await fetch(`${apiURL}/${user.username}/posts/${postId}/comments`, {
+            const response = await fetch(`${apiURL}/${user.username}/posts/${postId}/comments/${commentId ? commentId : ""}?page=${page}&take=${take}`, {
                 method: 'GET',
                 headers: {'Authorization': `Bearer ${token}`},
             });
@@ -46,12 +45,15 @@ function CommentList() {
                 throw new Error("Failed to Load Comments");
             }
             const data = await response.json();
-            setComments(data);
+            setData(data);
             setLoading(false);
+            if(page > data.pagination.totalPages) {
+                setPage(data.pagination.totalPages);
+            }
         } catch(err) {
             setError(err.message);
         }
-    }, [apiURL, token, user, changeToken, navigate, postId]);
+    }, [apiURL, token, user, changeToken, navigate, postId, page, commentId]);
 
     useEffect(() => {
         fetchComments();
@@ -78,74 +80,56 @@ function CommentList() {
             if(response.status >= 500) {
                 throw new Error("Failed to Delete Comment");
             }
-            setLoading(false);
             await fetchComments(); //refresh comments after deleting a comment
         } catch(err) {
             setError(err.message);
+        } finally {
+            setLoading(false);
         }
     }
+
+    function changePage(value) {
+        setPage(value);
+    }
+
+    console.log(data);
+    console.log(arr);
 
     if (error)   return <ServerErrorPage error={error}/>;
     if (loading) return <LoadPage message={"Loading..."}/>;
     return (
     <>
-        {comments.length === 0 && (
-            <>
-                <div className='h-75 d-flex flex-column align-items-center mt-5 text-dark display-3 fw-light gap-5' 
-                    style={{backgroundColor: '#9de1fcff'}}><span>No Comments</span>
-                     <button className='btn btn-primary btn-lg fs-4'
-                     onClick={() => navigate(-1)} type='button'>Back</button>
-                </div>
-            </>
-        )}
-        {comments.length != 0 && (
-            <div className='h-75'>
+        <div className='flex-grow-1 d-flex flex-column h-100'>
+            <div className="navbar py-4 d-flex flex-column flex-lg-row
+                align-items-center gap-3 py-3 justify-content-center">
                 <div className="p-2 d-flex justify-content-center">
                     <button className='btn btn-primary btn-lg fs-3'
                     onClick={() => navigate(-1)} type='button'>Back</button>
                 </div>
-                <div className="commentList h-100 p-2 container-fluid overflow-x-hidden p-4 overflow-y-auto pt-2">
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3 gx-3 mt-0">
-                        {comments.map((comment) => {
-                            return (
-                                <div className="col">
-                                    <div className='comment card bg-secondary text-white shadow-lg h-100' 
-                                    key={comment.id}>
-                                        <div className="card-body">
-                                            <div className="commentCreator card-title fs-4 text-center">{comment.creator.username}</div>
-                                            <div className="commentContent card-subtitle fs-4 text-center text-info overflow-auto"
-                                                style={{maxHeight: "20vh"}}>
-                                                {comment.content}
-                                            </div>
-                                            <div className="commentStats  fs-5 text-center d-grid">
-                                                <div className='row'>
-                                                    <span className="poststat col">Likes: {comment.CommentLikes.length}</span>
-                                                    <span className="poststat col">Replies: {comment.replies.length}</span>
-                                                </div>
-                                            </div>
-                                            <div className="commenttimes">
-                                                <div className="commentCreated text-center">
-                                                    Created: {formatDate(comment.createdAt)}
-                                                </div>
-                                            </div>
-                                            <div className="commentbuttons container-fluid mt-3">
-                                                <div className="row justify-content-center">
-                                                    {comment.replies.length != 0 &&<button className='col-6 text-center btn btn-primary'
-                                                    onClick={() => navigate(`/comment/${postId}/${comment.id}`)}>Replies</button>}
-                                                    <button className='col-6 text-center btn btn-danger'
-                                                    onClick={() => handleDelete(comment.id)}>Delete</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-                
             </div>
-        )}
+            <div className='flex-grow-1 overflow-auto' style={{ minHeight: 0 }}>
+                {arr.length === 0 && (
+                    <div className='d-flex flex-column align-items-center mt-5 text-dark display-3 fw-light gap-5'
+                        style={{backgroundColor: '#9de1fcff'}}><span>No Comments</span>
+                    </div>
+                )}
+                {arr.length != 0 && (
+                    <div className="commentList h-100 p-2 container-fluid overflow-x-hidden p-4 overflow-y-auto pt-2">
+                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3 gx-3 mt-0">
+                            {arr.map((comment) => {
+                                return (
+                                    <Comment postId={postId} commentId={comment.id}
+                                    creator={comment.creator} content={comment.content}
+                                    createdAt={comment.createdAt} likes={comment._count.CommentLikes}
+                                    replies={comment._count.replies} handleDelete={handleDelete}/>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </div>
+            <Pagination page={page} data={data} changePage={changePage} />
+        </div>
     </>
     );
 }
--- src/components/PostListDisplay.jsx
+++ src/components/PostListDisplay.jsx
@@ -4,28 +4,34 @@ import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import LoadPage from './LoadPage';
 import ServerErrorPage from './ServerErrorPage';
+import Post from './Post';
+import Pagination from './Pagination';
 
-const dateFmt = new Intl.DateTimeFormat(undefined, {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-});
-
-const formatDate = (d) => d ? dateFmt.format(new Date(d)) : '';
 
 function PostListDisplay() {
     const navigate = useNavigate();
     const {token, changeToken, user} = useContext(UserContext); 
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [posts, setPosts] = useState([]);
+    const [data, setData] = useState();
 
+    const [query, setQuery] = useState('');
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [published, setPublished] = useState(null);
+    const [uploadFilter, setUploadFilter] = useState("desc");
+
+    const [page, setPage] = useState(1);
     const apiURL = import.meta.env.VITE_API_URL;
+    const take = 25;
 
     const fetchPosts = useCallback(async () => {
         setLoading(true);
         try {
-            const response = await fetch(`${apiURL}/${user.username}/posts`, {
+            const baseURL = `${apiURL}/${user.username}/posts`;
+            const options = `?page=${page}&take=${take}&published=${published}&uploadFilter=${uploadFilter}`;
+            const finalURL = query === '' ? baseURL + options : baseURL + options + `&search=${query}`;
+            const response = await fetch(finalURL, {
                 method: 'GET',
                 headers: {'Authorization': `Bearer ${token}`},
             });
@@ -39,16 +45,19 @@ function PostListDisplay() {
                 throw new Error("Failed to Load Posts");
             }
             const data = await response.json();
-            setPosts(data);
+            setData(data);
             setLoading(false);
+            if(page > data.pagination.totalPages) {
+                setPage(data.pagination.totalPages);
+            }
         } catch(err) {
             setError(err.message);
         }
-    }, [apiURL, token, user, changeToken, navigate]);
+    }, [apiURL, token, user, changeToken, navigate, page, published, uploadFilter, query]);
 
     useEffect(() => {
         fetchPosts();
-    }, [fetchPosts]);
+    }, [fetchPosts, query, published, uploadFilter]);
 
     async function handleDelete(postid) {
         setLoading(true);
@@ -68,7 +77,7 @@ function PostListDisplay() {
                 navigate("/404notfound");
                 return;
             }
-            if(response.status >= 500) {
+            if(response.status >= 500 || !response.ok) {
                 throw new Error("Failed to Delete Post");
             }
             setLoading(false);
@@ -114,86 +123,113 @@ function PostListDisplay() {
             setError(err.message);
         }
     }
+
+    function changePublished(value) {
+        setPublished(value);
+        setPage(1);
+    }
+
+    function changeUploadFilter(value) {
+        setUploadFilter(value);
+        setPage(1);
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setPage(1);
+        setQuery(searchTerm);
+        /* await fetchData(); */
+        console.log("done");
+    }
+
+    function changePage(value) {
+        setPage(value);
+    }
+
     
     if (error)   return <ServerErrorPage error={error}/>;
     if (loading) return <LoadPage message={"Loading..."}/>;
 
+    console.log(data);
+
     return (
     <>
-        <div className="container-fluid py-4 d-flex flex-column flex-sm-row 
-        align-items-center gap-3 py-3">
-            <div className='text-black fs-6 d-none d-sm-flex align-items-center flex-sm-row 
-            flex-column justify-content-center'>
-                <span className='fs-5'>Welcome Back,</span>
-                <span className='text-bg-primary ms-2 p-2 rounded-pill fst-italic fw-bold overflow-auto'>
-                    {user.username}
-                </span>
-            </div>
-            <button className='btn btn-success py-3 fs-6 ms-sm-auto me-sm-5 btn-lg fw-bold'
-            onClick={() => navigate("/new")}>New Post</button>
-        </div>
-        {posts.length === 0 && (
-            <div className='h-75 d-flex justify-content-center mt-5 text-dark display-3 fw-light' 
-            style={{backgroundColor: '#9de1fcff'}}><span>No Posts</span>
-            </div>
-        )}
-        {posts.length != 0 && (
-            <div className="postList h-75 p-2 overflow-y-auto container-fluid overflow-x-hidden">
-                <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3 gx-3 mt-0 h-100'>
-                    {posts.map((post) => {
-                        return (
-                            <div className="col">
-                                <div className='post card h-100 bg-secondary text-white shadow-lg' key={post.id}>
-                                    <div className="card-body">
-                                        <div className="postTitle card-title fs-2 text-center">{post.title}</div>
-                                        <div className="poststats card-subtitle fs-5 text-center d-grid">
-                                            <div className="row">
-                                                <span className="poststat col">Likes: {post.PostLikes.length}</span>
-                                                <span className="poststat col">Comments: {post.comments.length}</span>
-                                            </div>
-                                        </div>
-                                        <div className="posttimes card-text my-1">
-                                            <div className="postupdated text-center">
-                                                Updated: {formatDate(post.updatedAt)}
-                                            </div>
-                                            <div className="postcreated text-center">
-                                                Created: {formatDate(post.createdAt)}
-                                            </div>
-                                        </div>
-                                        <div className="postbuttons container-fluid mt-3">
-                                            <div className="row g-2">
-                                                <div className="col-6 text-center">
-                                                    <button className='w-100 btn btn-primary'
-                                                    onClick={() => navigate(`/edit/${post.id}`)}>Edit</button>
-                                                </div>
-                                                <div className="col-6 text-center">
-                                                    <button className='w-100 btn btn-primary'
-                                                    onClick={() => navigate(`/comments/${post.id}`)}>Comments</button>
-                                                </div>
-                                                <div className="col-6 text-center">
-                                                    {post.published && (
-                                                        <button className='w-100 btn btn-warning'
-                                                        onClick={() => changePublish(post.id, false, post.title, post.content)}>Archive</button>
-                                                    )}
-                                                    {!post.published && (
-                                                        <button className='w-100 btn btn-success'
-                                                        onClick={() => changePublish(post.id, true, post.title, post.content)}>Publish</button>
-                                                    )}
-                                                </div>
-                                                <div className="col-6 text-center">
-                                                    <button className='w-100 btn btn-danger'
-                                                    onClick={() => handleDelete(post.id)}>Delete</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })}
+        <div className='flex-grow-1 d-flex flex-column h-100'>
+            <div className="navbar py-4 d-flex flex-column flex-lg-row
+            align-items-center gap-3 py-3 justify-content-center">
+                <div className="btn-group ms-sm-5">
+                    <button className={`btn btn-outline-success ${published === true ? 'btn-success text-black' : 'bg-white'}`}
+                        onClick={() => changePublished(true)}>
+                        Published
+                    </button>
+                    <button className={`btn btn-outline-info ${published === null ? 'btn-info text-black' : 'bg-white'}`}
+                        onClick={() => changePublished(null)}>
+                        All
+                    </button>
+                    <button className={`btn btn-outline-warning ${published === false ? 'btn-warning text-black' : 'bg-white'}`}
+                        onClick={() => changePublished(false)}>
+                        Archived
+                    </button>
+                </div>
+                <form className="form-inline row g-2 align-items-center justify-content-center flex-grow-1" onSubmit={handleSubmit}>
+                    <div className="col-10 col-sm-6">
+                        <input className="form-control me-sm-2" type="search" placeholder="Search" value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}/>
+                    </div>
+                    <div className="col-auto d-flex justify-content-center">
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                            Search
+                        </button>
+                            <button className="btn btn-outline-danger my-2 my-sm-0 me-0 ms-2" type="button"
+                                onClick={() => {
+                                    setQuery('');
+                                    setSearchTerm('');
+                                    setPublished(null);
+                                    setUploadFilter("desc");
+                                    setPage(1);
+                                }}>
+                                Clear Search
+                        </button>
+                    </div>
+                </form>
+                <div className='d-flex gap-3'>
+                    <div className="btn-group">
+                        <button className={`btn btn-outline-primary ${uploadFilter === "desc" ? 'active' : 'bg-white'}`}
+                            onClick={() => changeUploadFilter("desc")}>
+                            Latest
+                        </button>
+                        <button className={`btn btn-outline-primary ${uploadFilter === "asc" ? 'active' : 'bg-white'}`}
+                            onClick={() => changeUploadFilter("asc")}>
+                            Oldest
+                        </button>
+                    </div>
+                    <button className='btn btn-success py-3 fs-6 ms-sm-auto me-sm-5 btn-lg fw-bold'
+                    onClick={() => navigate("/new")}>New Post</button>
                 </div>
             </div>
-        )}
+            <div className='flex-grow-1 overflow-auto' style={{ minHeight: 0 }}>
+                {data && data.posts.length === 0 && (
+                    <div className='d-flex justify-content-center mt-1 text-dark display-3 fw-light'
+                    style={{backgroundColor: '#9de1fcff'}}><span>No Posts</span>
+                    </div>
+                )}
+                {data && data.posts.length != 0 && (
+                    <div className="postList p-2 overflow-y-auto container-fluid overflow-x-hidden mb-2">
+                        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3 gx-3 mt-0 h-100'>
+                            {data.posts.map((post) => {
+                                return (
+                                    <Post postId={post.id} title={post.title} likes={post._count.PostLikes} comments={post._count.comments} 
+                                    createdAt={post.createdAt} updatedAt={post.updatedAt} 
+                                    content={post.content} published={post.published} 
+                                    handleDelete={handleDelete} changePublish={changePublish}/>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </div>
+            <Pagination page={page} data={data} changePage={changePage} />
+        </div>
     </>
     );
 }
